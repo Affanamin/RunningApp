cfbcac52be98c96cfe46ad011c068b1533cd2cc0
Affan-Admin <affanaminn@gmail.com>
2022-05-27 18:29:16 +0500

    Tracking Service Basic Setup - Complete & Run - Part 11

--- app/src/main/java/com/androidappwp/runningapp/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/androidappwp/runningapp/ui/fragments/TrackingFragment.kt
@@ -1,5 +1,6 @@
 package com.androidappwp.runningapp.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,6 +8,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import com.androidappwp.runningapp.R
+import com.androidappwp.runningapp.other.Constants.ACTION_START_OR_RESUME_SERVICE
+import com.androidappwp.runningapp.services.TrackingService
 import com.androidappwp.runningapp.ui.viewmodels.MainViewModel
 import com.google.android.gms.maps.GoogleMap
 import dagger.hilt.android.AndroidEntryPoint
@@ -26,10 +29,24 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
 
         mapView.onCreate(savedInstanceState)
 
+        btnToggleRun.setOnClickListener {
+            sendCommandToService(ACTION_START_OR_RESUME_SERVICE)
+
+
+        }
+
         mapView.getMapAsync{
             map = it
         }
+
+
     }
+
+    private fun sendCommandToService(action: String) =
+        Intent(requireContext(), TrackingService::class.java).also {
+            it.action = action
+            requireContext().startService(it)
+        }
 
     // Life Cycle Functions
 
