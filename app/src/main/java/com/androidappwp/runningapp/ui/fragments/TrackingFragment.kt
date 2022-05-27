875eed461166a5c3ceb5fce8a24586f794474ac0
Affan-Admin <affanaminn@gmail.com>
2022-05-27 16:36:54 +0500

    Handling the Map's Lifecycle - Complete & Run - Part 10

--- app/src/main/java/com/androidappwp/runningapp/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/androidappwp/runningapp/ui/fragments/TrackingFragment.kt
@@ -8,7 +8,9 @@ import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import com.androidappwp.runningapp.R
 import com.androidappwp.runningapp.ui.viewmodels.MainViewModel
+import com.google.android.gms.maps.GoogleMap
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_tracking.*
 
 
 @AndroidEntryPoint
@@ -17,4 +19,55 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
 
     private val viewModel: MainViewModel by viewModels()
 
+    private var map: GoogleMap ?= null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mapView.onCreate(savedInstanceState)
+
+        mapView.getMapAsync{
+            map = it
+        }
+    }
+
+    // Life Cycle Functions
+
+    override fun onResume() {
+        super.onResume()
+        mapView?.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStop()
+    }
+
+
+    override fun onPause() {
+        super.onPause()
+        mapView?.onPause()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView?.onLowMemory()
+    }
+
+//    override fun onDestroy() {
+//        super.onDestroy()
+//        mapView?.onDestroy()
+//    }
+
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView?.onSaveInstanceState(outState)
+
+    }
 }
