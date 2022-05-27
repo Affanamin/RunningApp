bf5f8acd2ab0ca72d2d8414fa730527a38bb4d23
Affan-Admin <affanaminn@gmail.com>
2022-05-27 12:37:43 +0500

    Setting up Navigation - Complete & Run - Part 8

--- app/src/main/java/com/androidappwp/runningapp/ui/MainActivity.kt
+++ app/src/main/java/com/androidappwp/runningapp/ui/MainActivity.kt
@@ -2,23 +2,37 @@ package com.androidappwp.runningapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.androidappwp.runningapp.R
-import com.androidappwp.runningapp.db.RunDao
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
-
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    @Inject
-    lateinit var runDao: RunDao
-    //lateinit var runDao: RunDao
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //setSupportActionBar(toolbar)
+
+        bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
+
+        navHostFragment.findNavController()
+            .addOnDestinationChangedListener{ _,destination,_ ->
+                when(destination.id){
+                    R.id.settingsFragment, R.id.runFragment, R.id.statisticsFragment -> {
+                        bottomNavigationView.visibility = View.VISIBLE
+                    }
+                    else -> bottomNavigationView.visibility = View.GONE
+                }
+
+            }
+
+
 
     }
 }
--- app/src/main/java/com/androidappwp/runningapp/ui/fragments/SetupFragment.kt
+++ app/src/main/java/com/androidappwp/runningapp/ui/fragments/SetupFragment.kt
@@ -5,8 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.androidappwp.runningapp.R
-
+import kotlinx.android.synthetic.main.fragment_setup.*
 
 
 /**
@@ -15,5 +16,13 @@ import com.androidappwp.runningapp.R
  * create an instance of this fragment.
  */
 class SetupFragment : Fragment(R.layout.fragment_setup) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        tvContinue.setOnClickListener {
+            findNavController().navigate(R.id.action_setupFragment_to_runFragment)
+        }
+    }
 
 }
