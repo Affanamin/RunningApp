558f51a639b86f8e40e5afc5d34d14c2785f2398
Affan-Admin <affanaminn@gmail.com>
2022-05-31 14:38:19 +0500

    Drawing the Running Track on the Map - MVVM Running Tracker App - Complete & Run - Part 14

--- app/src/main/java/com/androidappwp/runningapp/other/Constants.kt
+++ app/src/main/java/com/androidappwp/runningapp/other/Constants.kt
@@ -1,5 +1,7 @@
 package com.androidappwp.runningapp.other
 
+import android.graphics.Color
+
 object Constants {
 
     const val RUNNING_DATABASE_NAME = "running_db"
@@ -14,4 +16,10 @@ object Constants {
     const val NOTIFICATION_ID = 1  // atleast to one
 
     const val ACTION_SHOW_TRACKING_FRAGMENT = "ACTION_SHOW_TRACKING_FRAGMENT"
+
+    const val LOCATION_UPDATE_INTERVAL = 5000L
+    const val FASTEST_LOCATION_INTERVAL = 2000L
+    const val POLY_LINE_COLOR = Color.RED
+    const val POLY_LINE_WIDTH = 8f
+    const val MAP_ZOOM = 15f
 }
--- app/src/main/java/com/androidappwp/runningapp/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/androidappwp/runningapp/ui/fragments/TrackingFragment.kt
@@ -7,11 +7,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import com.androidappwp.runningapp.R
+import com.androidappwp.runningapp.other.Constants.ACTION_PAUSE_SERVICE
 import com.androidappwp.runningapp.other.Constants.ACTION_START_OR_RESUME_SERVICE
+import com.androidappwp.runningapp.other.Constants.MAP_ZOOM
+import com.androidappwp.runningapp.other.Constants.POLY_LINE_COLOR
+import com.androidappwp.runningapp.other.Constants.POLY_LINE_WIDTH
+import com.androidappwp.runningapp.services.Polyline
 import com.androidappwp.runningapp.services.TrackingService
 import com.androidappwp.runningapp.ui.viewmodels.MainViewModel
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.PolylineOptions
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_tracking.*
 
@@ -22,6 +30,9 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
 
     private val viewModel: MainViewModel by viewModels()
 
+    private var isTracking = false
+    private var pathPoints = mutableListOf<Polyline>()
+
     private var map: GoogleMap ?= null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -30,16 +41,87 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
         mapView.onCreate(savedInstanceState)
 
         btnToggleRun.setOnClickListener {
-            sendCommandToService(ACTION_START_OR_RESUME_SERVICE)
-
-
+            //sendCommandToService(ACTION_START_OR_RESUME_SERVICE)
+            toggleRun()
         }
 
         mapView.getMapAsync{
             map = it
+            addAllPolyLines()
         }
 
+        subscribeToObservers()
 
+
+    }
+
+    private fun subscribeToObservers(){
+        TrackingService.isTracking.observe(viewLifecycleOwner, Observer {
+            updateTracking((it))
+        })
+
+        TrackingService.pathPoints.observe(viewLifecycleOwner, Observer {
+            pathPoints = it
+            addLatestPolyLine()
+            moveCameraToUser()
+        })
+    }
+
+    private fun toggleRun(){
+        if(isTracking){
+            sendCommandToService(ACTION_PAUSE_SERVICE)
+        }else{
+            sendCommandToService(ACTION_START_OR_RESUME_SERVICE)
+        }
+    }
+
+    private fun updateTracking(isTracking:Boolean){
+        this.isTracking = isTracking
+        if(!isTracking){
+            btnToggleRun.text = "Start"
+            btnFinishRun.visibility = View.VISIBLE
+        }else{
+            btnToggleRun.text = "Stop"
+            btnFinishRun.visibility = View.GONE
+        }
+
+    }
+
+
+    private fun moveCameraToUser(){
+        if(pathPoints.isNotEmpty() && pathPoints.last().isNotEmpty()){
+            map?.animateCamera(
+                CameraUpdateFactory.newLatLngZoom(
+                    pathPoints.last().last(),
+                    MAP_ZOOM
+
+                )
+            )
+        }
+    }
+
+    private fun addAllPolyLines(){
+        for(polyline in pathPoints){
+            val polylineOptions = PolylineOptions()
+                .color(POLY_LINE_COLOR)
+                .width(POLY_LINE_WIDTH)
+                .addAll(polyline)
+            map?.addPolyline(polylineOptions)
+        }
+    }
+
+    private fun addLatestPolyLine(){
+        if(pathPoints.isNotEmpty() && pathPoints.last().size > 1){
+            val preLastLatLng = pathPoints.last()[pathPoints.last().size-2]
+            val lastLatLng = pathPoints.last().last()
+            val polylineOptions = PolylineOptions()
+                .color(POLY_LINE_COLOR)
+                .width(POLY_LINE_WIDTH)
+                .add(preLastLatLng)
+                .add(lastLatLng)
+
+            map?.addPolyline(polylineOptions)
+        }
     }
 
     private fun sendCommandToService(action: String) =
